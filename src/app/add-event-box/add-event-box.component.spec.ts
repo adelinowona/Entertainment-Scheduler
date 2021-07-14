e762aea29c8ce77cc3a640a4ab34fdc350f924da
HackInversion <adelinowona@gmail.com>
2021-07-14 16:40:56 -0400

    Co-authored-by: Andy Pham <phama2@users.noreply.github.com>

---
+++ src/app/add-event-box/add-event-box.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AddEventBoxComponent } from './add-event-box.component';
+
+describe('AddEventBoxComponent', () => {
+  let component: AddEventBoxComponent;
+  let fixture: ComponentFixture<AddEventBoxComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AddEventBoxComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AddEventBoxComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/add-event-box/add-event-box.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-event-box',
+  templateUrl: './add-event-box.component.html',
+  styleUrls: ['./add-event-box.component.css']
+})
+export class AddEventBoxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
