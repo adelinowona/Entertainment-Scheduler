23fc75443c2b466c102fa145b4099a0a4c076cab
Andy Pham <70230763+phama2@users.noreply.github.com>
2021-08-08 19:12:38 -0400

    Revert "Added Legend visibility functionality"
    This reverts commit 62d28f48e90d06926c306d51588e5a16215e2984.

--- src/app/components/side-options/side-options.component.ts
+++ src/app/components/side-options/side-options.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { UiService } from 'src/app/services/ui.service';
-import { HomeComponent } from '../home/home.component';
-
 
 @Component({
   selector: 'app-side-options',
@@ -9,8 +7,8 @@ import { HomeComponent } from '../home/home.component';
   styleUrls: ['./side-options.component.css']
 })
 export class SideOptionsComponent implements OnInit {
-  
-  constructor(private uiService: UiService, private homeComponent: HomeComponent) {}
+
+  constructor(private uiService: UiService) {}
 
   ngOnInit(): void {
   }
@@ -25,21 +23,7 @@ export class SideOptionsComponent implements OnInit {
     this.uiService.openTimeTable();
   }
 
-  checkVisible($event: any, color: number){
-    let colors: {[key:number]:string} = {};
-    colors =
-    {
-      1: '#FF2626',
-      2: '#FF8C26',
-      3: '#FBBC04',
-      4: '#04F100',
-      5: '#3390FF',
-      6: '#AF6CFF',
-      7: '#E334FF'
-    }
-    let actual = colors[color];
-    this.homeComponent.toggleVisibility($event.checked, actual);
+  toggleVisibility(event: any){
+    // to be implemented
   }
-
-  
 }
